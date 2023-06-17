8ac3ffaeb7e523c8a30ae16c047d5af4bf91d1a8
Cal <obinnaanizoba5@gmail.com>
2023-06-17 23:40:42 +0800

    heaps

---
+++ problem-solving/DataStructures/Heaps/heaps.js
@@ -0,0 +1,29 @@
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [41, 39, 33, 18, 27, 12];
+  }
+
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+    return this.values;
+  }
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+      if (element <= parent) break;
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
+}
+
+//[41,39,33,18,27,12]
+
+const myHeap = new MaxBinaryHeap();
+myHeap.insert(55);
+myHeap.insert(100);
