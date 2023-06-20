39dde8a6bd81744c05962a3c36994b3c3ac63e36
Cal <obinnaanizoba5@gmail.com>
2023-06-20 21:21:59 +0800

    update

--- problem-solving/DataStructures/Heaps/heaps.js
+++ problem-solving/DataStructures/Heaps/heaps.js
@@ -1,65 +1,68 @@
 class MaxBinaryHeap {
-  constructor() {
-    this.values = [41, 39, 33, 18, 27, 12];
+  constructor(){
+      this.values = [41,39,33,18,27,12];
   }
 
-  insert(element) {
-    this.values.push(element);
-    this.bubbleUp();
-    return this.values;
+  insert(element){
+      this.values.push(element);
+      this.bubbleUp();
+      return this.values;
   }
-  bubbleUp() {
-    let index = this.values.length - 1;
-    const element = this.values[index];
-    while (index > 0) {
-      let parentIndex = Math.floor((index - 1) / 2);
-      let parent = this.values[parentIndex];
-      if (element <= parent) break;
-      this.values[parentIndex] = element;
-      this.values[index] = parent;
-      index = parentIndex;
-    }
-  }
-  extractMax() {
-    const max = this.values[0];
-    const lastElement = this.values.length - 1;
-    this.values[0] = this.values[lastElement];
-    this.sinkDown();
-    return max;
-  }
-  sinkDown() {
-    let index = 0;
-    const length = this.values.length;
-    const element = this.values[0];
-    while (true) {
-      let leftChildIndex = 2 * index + 1;
-      let rightChildIndex = 2 * index + 1;
-      let leftChild, rightChild;
-      let swap = null;
-
-      if (leftChildIndex < length) {
-        leftChild = this.values[leftChildIndex];
-        if (leftChild > element) {
-          swap = leftChildIndex;
-        }
+  bubbleUp(){
+      let index = this.values.length - 1;
+      const element = this.values[index];
+      while(index>0){
+          let parentIndex = Math.floor((index-1)/2);
+          let parent = this.values[parentIndex];
+          if(element <= parent) break;
+          this.values[parentIndex] = element;
+          this.values[index] = parent;
+          index = parentIndex;
       }
-      if (rightChildIndex < length) {
-        rightChild = this.values[rightChildIndex];
-        if (
-          (swap === null && rightChild > element) ||
-          (swap !== null && rightChild > leftChild)
-        ) {
-          swap = rightChildIndex;
-        }
+  }
+  extractMax(){
+      const max = this.values[0];
+      if(this.values.length === 0) return null;
+      if(this.values.length === 1){
+          this.values = [];
+          return max;
       }
+      const lastElement = this.values.pop();
+      this.values[0] = lastElement;
+      this.sinkDown();
+      return max;
+  }
+  sinkDown(){
+      let index = 0;
+      const length = this.values.length;
+      const element = this.values[0];
+      while(true){
+          let leftChildIndex = 2 * index + 1;
+          let rightChildIndex = 2 * index + 1;
+          let leftChild, rightChild;
+          let swap = null;
 
-      if (swap === null) break;
-      this.values[index] = this.values[swap];
-      this.values[swap] = element;
-      index = swap;
-    }
+          if(leftChildIndex < length){
+              leftChild = this.values[leftChildIndex];
+              if(leftChild > element){
+                  swap = leftChildIndex;
+              }
+          }
+          if(rightChildIndex < length){
+              rightChild = this.values[rightChildIndex];
+              if((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)){
+                  swap = rightChildIndex;
+              }
+          }
+
+          if(swap === null) break;
+          this.values[index] = this.values[swap];
+          this.values[swap] = element;
+          index = swap;
+      }
   }
 }
+
 
 //[41,39,33,18,27,12]
 
@@ -68,5 +71,13 @@ myHeap.insert(55);
 myHeap.insert(100);
 myHeap.insert(200);
 
+myHeap.extractMax();
+myHeap.extractMax();
+myHeap.extractMax();
+myHeap.extractMax();
+myHeap.extractMax();
+myHeap.extractMax();
+myHeap.extractMax();
+myHeap.extractMax();
 myHeap.extractMax();
 myHeap.extractMax();
